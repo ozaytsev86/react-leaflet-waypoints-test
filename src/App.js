e8fb6eab87de00a45777294f695d8436baccfa69
olek <aleksander.zaytsev@gmail.com>
2021-11-08 23:53:47 +0100

    update react-leaflet-waypoints dependency

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './app.css';
-import {ReactLeafletMap} from 'react-leaflet-waypoints';
+import {ReactLeafletWaypoints} from 'react-leaflet-waypoints';
 
 function App() {
   return (
     <div className="App">
-      <ReactLeafletMap
+      <ReactLeafletWaypoints
         layerUrl="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         waypoints={[{lat: '40.4381311', lng: '-3.8196196'}, {lat: '42.7576862', lng: '1.5082874'}]}
       />
